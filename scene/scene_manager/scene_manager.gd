278140ab4bfc309741a6103daabc76476c038a9b
Dukantic <49958988+Dukantic@users.noreply.github.com>
2025-01-14 17:43:31 +0100

    scene manager

---
+++ scene/scene_manager/scene_manager.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@export var first_scene :PackedScene = null 
+
+signal change_scene_signal(new_scene: PackedScene)
+
+var current_scene:Node = null
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	change_scene_signal.connect(change_scene_func)
+	emit_signal("change_scene_signal", first_scene)
+
+
+func change_scene_func(scene: PackedScene) -> void:
+	if current_scene != null:
+		current_scene.queue_free()
+	current_scene = scene.instantiate()
+	add_child(current_scene)
